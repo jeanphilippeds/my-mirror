9b14b6a8e6f1b13542e6240922ecd7e41d6a8166
Jean-Philippe Dos Santos <jeanphilippe_ds@MacBook-Pro-de-JP.local>
2017-09-10 23:03:41 +0200

    Add stations

--- client/src/pages/Home/Home.component.js
+++ client/src/pages/Home/Home.component.js
@@ -6,15 +6,29 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      lamarck: [],
+      guymoquet: [],
+      carpeaux: []
     };
   }
 
   componentDidMount() {
     axios.get(`https://api.jcdecaux.com/vls/v1/stations/18047?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
     .then(res => {
-      const posts = JSON.stringify(res.data);
-      this.setState({ posts });
+      const lamarck = JSON.stringify(res.data.available_bikes);
+      this.setState({ lamarck });
+    });
+
+    axios.get(`https://api.jcdecaux.com/vls/v1/stations/17001?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
+    .then(res => {
+      const guymoquet = JSON.stringify(res.data.available_bikes);
+      this.setState({ guymoquet });
+    });
+
+    axios.get(`https://api.jcdecaux.com/vls/v1/stations/18018?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
+    .then(res => {
+      const carpeaux = JSON.stringify(res.data.available_bikes);
+      this.setState({ carpeaux });
     });
   }
 
@@ -23,7 +37,16 @@ export default class Home extends Component {
       <div>
         <p className="intro"/>
         <p>
-          {this.state.posts}
+          Vélos dispos [Lamarck]:
+          {this.state.lamarck}
+        </p>
+        <p>
+          Vélos dispos [Guy-Moquet]:
+          {this.state.guymoquet}
+        </p>
+        <p>
+          Vélos dispos [Carpeaux]:
+          {this.state.carpeaux}
         </p>
       </div>
     );
