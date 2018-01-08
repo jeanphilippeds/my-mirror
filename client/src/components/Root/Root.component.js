1f10f89d4ef9ecc0116ce98e917952d8aabf95e5
Jean-Philippe Dos Santos <jeanphilippe_ds@MacBook-Pro-de-JP.local>
2018-01-08 23:53:57 +0100

    Fix design

--- client/src/components/Root/Root.component.js
+++ client/src/components/Root/Root.component.js
@@ -1,19 +1,9 @@
-
 import React, { Component } from 'react';
-import { IntlProvider, addLocaleData } from 'react-intl';
 import axios from 'axios';
-import fr from 'react-intl/locale-data/fr';
-import frMessages from '../../translations/fr.json';
-import { flattenMessages } from '../../utils/i18n/intl';
 import map from './map.png';
 import bike from './bike.png';
+import metro from './metro.png';
 import './style.css';
-
-const locales = {
-  fr: flattenMessages(frMessages),
-};
-
-addLocaleData([...fr]);
 
 export default class Root extends Component {
   constructor() {
@@ -22,71 +12,60 @@ export default class Root extends Component {
   }
 
   componentDidMount() {
-        axios.get(`https://api.jcdecaux.com/vls/v1/stations/18047?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
-        .then(res => {
-          const lamarck = JSON.stringify(res.data.available_bikes);
-          this.setState({ lamarck });
-        });
+    setInterval(() => {
+      axios.get(`https://api.apixu.com/v1/forecast.json?key=05918f81db8d4f9d87b194259172310&q=Paris`)
+      .then(res => {
+        const temp = JSON.stringify(res.data.current.temp_c);
+        const humidity = JSON.stringify(res.data.current.humidity);
+        const condition = JSON.stringify(res.data.current.condition.text);
+        const tmin = JSON.stringify(Math.round(res.data.forecast.forecastday[0].day.mintemp_c));
+        const tmax = JSON.stringify(Math.round(res.data.forecast.forecastday[0].day.maxtemp_c));
+        this.setState({ temp });
+        this.setState({ humidity });
+        this.setState({ condition });
+        this.setState({ tmin });
+        this.setState({ tmax });
+      });
 
-        axios.get(`https://api.jcdecaux.com/vls/v1/stations/17001?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
-        .then(res => {
-          const guymoquet = JSON.stringify(res.data.available_bikes);
-          this.setState({ guymoquet });
-        });
-
-        axios.get(`https://api.jcdecaux.com/vls/v1/stations/18046?contract=Paris&apiKey=59e904bbe66668002da4aa7a2c93819cf98ca568`)
-        .then(res => {
-          const lafourche = JSON.stringify(res.data.available_bikes);
-          this.setState({ lafourche });
-        });
-        axios.get(`https://api.apixu.com/v1/forecast.json?key=05918f81db8d4f9d87b194259172310&q=Paris`)
-        .then(res => {
-          const temp = JSON.stringify(res.data.current.temp_c);
-          const humidity = JSON.stringify(res.data.current.humidity);
-          const condition = JSON.stringify(res.data.current.condition.text);
-          const tmin = JSON.stringify(res.data.forecast.forecastday[0].day.mintemp_c);
-          const tmax = JSON.stringify(res.data.forecast.forecastday[0].day.maxtemp_c);
-          this.setState({ temp });
-          this.setState({ humidity });
-          this.setState({ condition });
-          this.setState({ tmin });
-          this.setState({ tmax });
-        });
+      axios.get(`https://api-ratp.pierre-grimaud.fr/v3/schedules/metros/13/guy+moquet/R?_format=json`)
+      .then(res => {
+        const nextSubway = JSON.stringify(res.data.result.schedules[0].message).replace(/"/g, "");
+        const secondSubway = JSON.stringify(res.data.result.schedules[1].message).replace(/"/g, "");
+        this.setState({ nextSubway });
+        this.setState({ secondSubway });
+      });
+    }, 5000);
   }
 
   render() {
     return (
-      <IntlProvider locale="fr" messages={locales.fr} >
-        <div className="App">
-          <h2 className="App-title">Hello Pluton.</h2>
-          <div className="App-gradient">
-            <img src={bike} className="App-bike-1"/>
-            <img src={bike} className="App-bike-2"/>
-            <img src={bike} className="App-bike-3"/>
-            <img src={map} className="App-logo"/>
-          </div>
-          <div>
-            <p>
-              Vélos dispos [Lamarck]: {this.state.lamarck}
-            </p>
-            <p>
-              Vélos dispos [Guy-Moquet]: {this.state.guymoquet}
-            </p>
-            <p>
-              Vélos dispos [La Fourche]: {this.state.lafourche}
-            </p>
-            <p>
-              Température: {this.state.temp}°C [{this.state.tmin}°C - {this.state.tmax}°C]
-            </p>
-            <p>
-              Humidité: {this.state.humidity}%
-            </p>
-            <p>
-              Condition: {this.state.condition}
-            </p>
-          </div>
+      <div className="App">
+        <h2 className="App-title">Hello there.</h2>
+        <p className="App-text">
+          Il fait actuellement {this.state.temp}°C.
+        </p>
+        <p className="App-text">
+          L'humidité ambiante est de {this.state.humidity}%.
+        </p>
+        <p className="App-text">
+          Aujourd'hui, les températures s'élèveront de {this.state.tmin}°C à {this.state.tmax}°C.
+        </p>
+        <div>
+          <img src={metro} className="App-metro-icon"/>
+          <p className="App-metro-text">
+            Direction Chatillon-Montrouge: {this.state.nextSubway}, {this.state.secondSubway}
+          </p>
         </div>
-      </IntlProvider>
+        <p className="App-conclusion">
+          Bonne journée :)
+        </p>
+        <div className="App-gradient">
+          <img src={bike} className="App-bike-1"/>
+          <img src={bike} className="App-bike-2"/>
+          <img src={bike} className="App-bike-3"/>
+          <img src={map} className="App-logo"/>
+        </div>
+      </div>
     );
   }
 }
